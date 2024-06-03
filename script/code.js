4ed6e03b76d972b1aed7d014e3e52fd3cee9d8d5
AChar1f <acharif41@gmail.com>
2024-06-03 18:18:16 +0200

    exercise

---
+++ script/code.js
@@ -0,0 +1,24 @@
+// 1
+// let firstnumber = 10
+// let secondnumber = 15
+// let temp
+
+// temp = firstnumber
+// firstnumber = secondnumber
+// secondnumber = temp
+// console.log(firstnumber, secondnumber)
+
+// 2 
+// let firstnumber = 10
+// let secondnumber = 15
+// console.log(firstnumber, secondnumber)
+// firstnumber = firstnumber ^ secondnumber;
+// secondnumber = firstnumber ^ secondnumber;
+// firstnumber = firstnumber ^ secondnumber;
+// console.log(firstnumber, secondnumber)
+
+// 3
+// let firstnumber = 10;
+// let secondnumber = 15;
+// [firstnumber, secondnumber] = [secondnumber, firstnumber];
+// console.log(firstnumber, secondnumber)
